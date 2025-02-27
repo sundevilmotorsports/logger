eba0c27f0a8732e34c8e740f96acc94f6b2cffdd
Alexander-Rumer <78566239+asrumer@users.noreply.github.com>
2025-02-26 20:51:26 -0700

    (UNTESTED) Log Filename Strings 1st Commit

--- Core/Src/eeprom.c
+++ Core/Src/eeprom.c
@@ -3,6 +3,9 @@
  *
  *  Created on: Mar 18, 2024
  *      Author: joshl
+ * 	
+ *	Edited on: Feb 26, 2025
+ *		Author: Alex Rumer
  */
 #include "eeprom.h"
 
@@ -13,4 +16,13 @@ HAL_StatusTypeDef eepromWrite(I2C_HandleTypeDef* hi2c, uint16_t addr, uint8_t* d
 HAL_StatusTypeDef eepromRead(I2C_HandleTypeDef* hi2c, uint16_t addr, uint8_t* data) {
 	HAL_StatusTypeDef ret = HAL_I2C_Mem_Read(hi2c, EEPROM_DEV_ID | 0, addr, 2, data, 1, 1000);
 	return ret;
+}
+
+HAL_StatusTypeDef eepromReadString(I2C_HandleTypeDef* hi2c, uint16_t addr, char* data, uint8_t len){
+	HAL_StatusTypeDef ret = HAL_I2C_Mem_Read(hi2c, EEPROM_DEV_ID | 0, addr, 2, data, len, 1000);
+	return ret;
+}
+
+HAL_StatusTypeDef eepromWriteString(I2C_HandleTypeDef* hi2c, uint16_t addr, char* data, uint8_t len){
+	return HAL_I2C_Mem_Write(hi2c, EEPROM_DEV_ID | 0, addr, 2, data, len, 1000);
 }
