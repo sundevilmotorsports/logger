99b8bfd23edcf46a5deff0007f8c697188f56ab7
Nolan Peterson <ndpete2007@gmail.com>
2026-04-30 19:08:25 -0700

    sdmmc card implementation

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,32 @@
-fn main() {
-    // It is necessary to call this function once. Otherwise, some patches to the runtime
-    // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
-    esp_idf_svc::sys::link_patches();
+mod sd;
 
-    // Bind the log crate to the ESP Logging facilities
+use esp_idf_svc::hal::gpio::{AnyInputPin, AnyOutputPin};
+use esp_idf_svc::hal::peripherals::Peripherals;
+use esp_idf_svc::hal::rmt::ClockSource::Default;
+use esp_idf_svc::hal::sd::SdCardConfiguration;
+use log::info;
+
+fn main() -> anyhow::Result<()> {
+    esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    log::info!("Hello, world!");
+    info!("Hello, world!");
+
+    let peripherals = Peripherals::take()?;
+
+    // TODO: use correct pins
+    let sd = sd::SdCard::new(
+        peripherals.sdmmc0,
+        peripherals.pins.gpio4,
+        peripherals.pins.gpio5,
+        peripherals.pins.gpio6,
+        peripherals.pins.gpio7,
+        peripherals.pins.gpio8,
+        peripherals.pins.gpio9,
+        AnyInputPin::none(),
+        AnyInputPin::none(),
+        &SdCardConfiguration::default(),
+    )?;
+
+    Ok(())
 }
