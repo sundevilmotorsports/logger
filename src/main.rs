10841fe57df2e6ab17295554316433ac6c7dd7ec
Nolan Peterson <ndpete2007@gmail.com>
2026-05-01 22:56:55 -0700

    use embassy for async tasks

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,27 @@ mod can;
 mod configuration;
 mod sd;
 
-use can::{CanBus, CanDevice, Signal, SignalGroup, Signals};
-use configuration::Configuration;
+use can::CanBus;
+use embassy_time::Timer;
 use esp_idf_svc::hal::delay::FreeRtos;
-use esp_idf_svc::hal::gpio::{AnyInputPin, InterruptType, PinDriver, Pull};
+use esp_idf_svc::hal::gpio::{AnyInputPin, Input, PinDriver, Pull};
 use esp_idf_svc::hal::peripherals::Peripherals;
 use esp_idf_svc::hal::sd::SdCardConfiguration;
 use esp_idf_svc::hal::spi::config::Config as SpiConfig;
 use esp_idf_svc::hal::spi::{SpiDeviceDriver, SpiDriver, SpiDriverConfig};
 use log::info;
 use sd::SdCard;
-use std::sync::{mpsc, Arc, Mutex};
-use std::time::Duration;
+use static_cell::StaticCell;
+use std::sync::{Arc, Mutex};
 use crate::configuration::CONFIGURATION;
 
-fn main() -> anyhow::Result<()> {
+type CanBusType = CanBus<SpiDeviceDriver<'static, SpiDriver<'static>>>;
+
+fn main() {
     esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    let peripherals = Peripherals::take()?;
+    let peripherals = Peripherals::take().expect("failed to take peripherals");
 
     // TODO: use correct pins
     let _sd = SdCard::new(
@@ -34,7 +36,8 @@ fn main() -> anyhow::Result<()> {
         AnyInputPin::none(),
         AnyInputPin::none(),
         &SdCardConfiguration::default(),
-    )?;
+    )
+    .expect("SD card init failed");
 
     // TODO: use correct pins
     let spi = SpiDriver::new(
@@ -43,41 +46,45 @@ fn main() -> anyhow::Result<()> {
         peripherals.pins.gpio11,       // MOSI
         Some(peripherals.pins.gpio12), // MISO
         &SpiDriverConfig::new(),
-    )?;
-    let spi_device = SpiDeviceDriver::new(spi, Some(peripherals.pins.gpio13), &SpiConfig::new())?;
+    )
+    .expect("SPI driver init failed");
+    let spi_device = SpiDeviceDriver::new(spi, Some(peripherals.pins.gpio13), &SpiConfig::new())
+        .expect("SPI device init failed");
 
     let mut delay = FreeRtos;
-    let mut bus =
-        CanBus::new(spi_device, &mut delay).map_err(|_| anyhow::anyhow!("CAN init failed"))?;
-    
+    let mut bus = CanBus::new(spi_device, &mut delay).expect("CAN init failed");
+
     // TODO: load from config file on SD card
     for device in &*CONFIGURATION.lock().unwrap().can_devices {
         bus.register_can_device(device.clone());
     }
 
+    let int_pin = PinDriver::input(peripherals.pins.gpio14, Pull::Up) // TODO: correct INT pin
+        .expect("INT pin init failed");
+
     let bus = Arc::new(Mutex::new(bus));
 
-    let (int_tx, int_rx) = mpsc::sync_channel::<()>(1);
-    let mut int_pin = PinDriver::input(peripherals.pins.gpio14, Pull::Up)?; // TODO: correct INT pin
-    int_pin.set_interrupt_type(InterruptType::NegEdge)?;
-    unsafe {
-        int_pin.subscribe(move || {
-            int_tx.try_send(()).ok();
-        })?;
+    static EXECUTOR: StaticCell<embassy_executor::Executor> = StaticCell::new();
+    let executor = EXECUTOR.init(embassy_executor::Executor::new());
+    executor.run(|spawner| {
+        spawner.spawn(can_poll_task(int_pin, Arc::clone(&bus)).expect("can_poll_task"));
+        spawner.spawn(log_task(bus).expect("log_task"));
+    });
+}
+
+#[embassy_executor::task]
+async fn can_poll_task(mut int_pin: PinDriver<'static, Input>, bus: Arc<Mutex<CanBusType>>) {
+    loop {
+        int_pin.wait_for_falling_edge().await.ok();
+
+        if let Err(e) = bus.lock().unwrap().poll_once() {
+            log::warn!("CAN poll error: {:?}", e);
+        }
     }
-    int_pin.enable_interrupt()?;
+}
 
-    let bus_rx = Arc::clone(&bus);
-    std::thread::Builder::new()
-        .stack_size(8192)
-        .spawn(move || {
-            //let _int_pin = int_pin; // keep pin and subscription alive, might be needed
-            loop {
-                int_rx.recv().ok();
-                bus_rx.lock().unwrap().poll_once().ok();
-            }
-        })?;
-
+#[embassy_executor::task]
+async fn log_task(bus: Arc<Mutex<CanBusType>>) {
     loop {
         {
             let guard = bus.lock().unwrap();
@@ -89,6 +96,6 @@ fn main() -> anyhow::Result<()> {
             }
         }
 
-        std::thread::sleep(Duration::from_millis(100));
+        Timer::after_millis(50).await;
     }
 }
