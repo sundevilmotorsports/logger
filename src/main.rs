9e26b358a48ff9b5fa4a97f2bcc1e257ffe657cb
Nolan Peterson <ndpete2007@gmail.com>
2026-07-11 21:00:13 -0500

    feat: gnss

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod can;
 mod configuration;
+mod gnss;
 mod sd;
 mod serial;
 
@@ -8,11 +9,13 @@ use can::{can_poll_task, CanBusType};
 use configuration::{Configuration, CONFIGURATION};
 use embassy_time::Timer;
 use esp_idf_svc::hal::delay::FreeRtos;
-use esp_idf_svc::hal::gpio::{AnyInputPin, PinDriver, Pull};
+use esp_idf_svc::hal::gpio::{AnyIOPin, AnyInputPin, PinDriver, Pull};
 use esp_idf_svc::hal::peripherals::Peripherals;
 use esp_idf_svc::hal::sd::SdCardConfiguration;
 use esp_idf_svc::hal::spi::config::Config as SpiConfig;
 use esp_idf_svc::hal::spi::{SpiDeviceDriver, SpiDriver, SpiDriverConfig};
+use esp_idf_svc::hal::uart::{config as uart_config, UartDriver};
+use esp_idf_svc::hal::units::Hertz;
 use esp_idf_svc::hal::usb_serial::{UsbSerialConfig, UsbSerialDriver};
 use log::info;
 use sd::SdCard;
@@ -26,19 +29,19 @@ fn main() {
 
     let peripherals = Peripherals::take().expect("failed to take peripherals");
 
-    let _sd = SdCard::new(
-        peripherals.sdmmc0,
-        peripherals.pins.gpio44, // cmd
-        peripherals.pins.gpio43, // clk
-        peripherals.pins.gpio39, // d0
-        peripherals.pins.gpio40, // d1
-        peripherals.pins.gpio41, // d2
-        peripherals.pins.gpio42, // d3
-        AnyInputPin::none(),
-        AnyInputPin::none(),
-        &SdCardConfiguration::default(),
-    )
-    .expect("SD card init failed");
+    // let _sd = SdCard::new(
+    //     peripherals.sdmmc0,
+    //     peripherals.pins.gpio44, // cmd
+    //     peripherals.pins.gpio43, // clk
+    //     peripherals.pins.gpio39, // d0
+    //     peripherals.pins.gpio40, // d1
+    //     peripherals.pins.gpio41, // d2
+    //     peripherals.pins.gpio42, // d3
+    //     AnyInputPin::none(),
+    //     AnyInputPin::none(),
+    //     &SdCardConfiguration::default(),
+    // )
+    // .expect("SD card init failed");
 
     Configuration::init().expect("config init failed");
 
@@ -63,6 +66,17 @@ fn main() {
     //
     // let int_pin = PinDriver::input(peripherals.pins.gpio14, Pull::Up) // TODO: correct INT pin
     //     .expect("INT pin init failed");
+
+    let gnss_uart = UartDriver::new(
+        peripherals.uart1,
+        peripherals.pins.gpio33, // TX
+        peripherals.pins.gpio32, // RX
+        Option::<AnyIOPin>::None,
+        Option::<AnyIOPin>::None,
+        &uart_config::Config::new().baudrate(Hertz(38_400)),
+    )
+    .expect("GNSS UART init failed");
+    gnss::spawn_reader(gnss_uart);
 
     let usb_serial = UsbSerialDriver::new(
         peripherals.usb_serial,
